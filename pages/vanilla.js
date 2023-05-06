3c9c3fd10ccc1e51d169da25f04796dd0bf00d6c
laszej <stanislawlaskowski87@gmail.com>
2023-05-06 22:10:03 +0200

    l

--- pages/vanilla.js
+++ pages/vanilla.js
@@ -90,7 +90,7 @@ function Vanilla() {
         <div className={`${styles.projectsWrapper} ${isVisible[1] ? styles["is-visible"] : ""}`} ref={refs[1]}>
         <div className={styles.projects}><p><strong>Simple Chat App</strong><br/><br/>Prosty czat oparty na popularnym json-server, który zainstalowałem w serwisie Heroku. Heroku zlikwidowało free plan, więc przerobiłem na podstronę nexta, w którym napisałem prosty backend. Aplikacja na Android nie będzie już działać.   </p>
             <div className={styles.leftRight}>
-                <div>Przetestuj<Link href='https://budowa.vercel.app/simplechat' target='blank'><strong> tutaj
+                <div>Przetestuj<Link href='https://stanislawlaskowski.vercel.app/simplechat' target='blank'><strong> tutaj
                  </strong></Link></div>
                  <div>Pobierz na Android<Link href='https://drive.google.com/drive/folders/1z1aDtfu5iPta1-jawEi4ps9mtVjwDNtZ?usp=sharing' target='blank'><strong> tutaj</strong></Link></div></div><img src='/projects/chat.png'className={styles.pImages} /></div></div>
 
